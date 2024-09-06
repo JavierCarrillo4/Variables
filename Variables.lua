b21a17fe98acc44001cff8f8fe04dfb6d3271c56
JavierCarrillo4 <javiercarrillo424@gmail.com>
2024-09-06 02:37:27 -0400

    Add files via upload

---
+++ Variables.lua
@@ -0,0 +1,14 @@
+local HelloVariable = "Hello!" -- local creates a private variable HelloVariable and
+							   -- assigns the string "Hello!"
+
+print(HelloVariable) -- prints the value (string) of HelloVariable
+
+HelloVariable = "New String" -- assigns "New String" string to HelloVariable
+
+print(HelloVariable) -- prints the value (string) of HelloVariable
+
+local booleanVariable = false -- creates a local variable booleanVariable
+							  -- and assigns the boolean value false
+
+local integerVariable = 10	-- creates a local variable integerVariable
+							-- and assigns the integer value false
